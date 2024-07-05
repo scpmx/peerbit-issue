2572d86be232b9b1bc3757bb02e5efaa5dd960f1
Stephen <13601394+scpmx@users.noreply.github.com>
2024-07-05 16:44:26 -0400

    Show static site issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,16 @@
 
     async function init() {
         var pb = await Peerbit.create();
+
+        return pb.peerId.toString();
     }
 </script>
+
+{#await init()}
+    <p>loading...</p>
+{:then x} 
+    <p>{x}</p>
+{/await}
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
